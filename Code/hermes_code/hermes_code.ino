fb10b38af238f802c52cfc16b235bba5d0c265d0
raviwhitehat <ekravi26@gmail.com>
2026-06-10 21:20:20 +0530

    Add new components and update code for temperature and distance measurement
    - Updated fp-lib-table and sym-lib-table to include new libraries: LM393, DHT11, BUTT-2, HC-SR04, and ARDUINO_NANO.
    - Added LM393 component files including symbol and footprint.
    - Created a redirection HTML file for LM393 import instructions.
    - Modified the Arduino code to integrate DHT11 sensor for temperature and humidity readings.
    - Implemented button functionality to toggle between temperature and distance display modes.
    - Updated display logic to show sensor data based on the selected mode.

--- Code/hermes_code/hermes_code.ino
+++ Code/hermes_code/hermes_code.ino
@@ -2,23 +2,39 @@
 #include<Adafruit_VL53L0X.h>
 #include<Adafruit_GFX.h>
 #include<Adafruit_SSD1306.h>
+#include<DHT.h>
 
 #define SCREEN_WIDTH 128
 #define SCREEN_HEIGHT 64
 #define OLED_RESET -1
 #define OLED_ADDR 0x3c
+#define TRIG_PIN 9
+#define ECHO_PIN 10
+#define DHT_PIN A1
+#define BTN_PIN 3
+#define DHTTYPE DHT11
 
 Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, OLED_RESET);
 
-Adafruit_VL53L0X sensor_config = Adafruit_VL53L0X();
+DHT dht(DHT_PIN,DHTTYPE);
+
+// Adafruit_VL53L0X sensor_config = Adafruit_VL53L0X();
+
+bool temp_state = 0; //State in which Screen Shows temperature data
+bool lastBtnnState = HIGH;
+bool currentBtnState = HIGH;
 
 void setup(){
   Serial.begin(9600);
   Wire.begin();
+  dht.begin();
+
+  pinMode(BTN_PIN, INPUT_PULLUP);
+  pinMode(IR_PIN, INPUT);
 
   if(!display.begin(SSD1306_SWITCHCAPVCC, OLED_ADDR)){
     Serial.println("OLED Display Intialization failed.");
-    // while(1);
+    while(1);
   }
 
   display.clearDisplay();
@@ -29,24 +45,61 @@ void setup(){
   display.display();
   delay(500);
 
-  if(!sensor_config.begin()){
-    Serial.println("Failed to Load the IR Sensor");
-    display.clearDisplay();
-    display.setCursor(0, 10);
-    display.println("Sensor Error");
-    display.display();
-    // while(1);
-  }
+  // if(!sensor_config.begin()){
+  //   Serial.println("Failed to Load the IR Sensor");
+  //   display.clearDisplay();
+  //   display.setCursor(0, 10);
+  //   display.println("Sensor Error");
+  //   display.display();
+  //   // while(1);
+  // }
 }
 
 void loop() {
-  VL53L0X_RangingMeasurementData_t measure;
 
-  sensor_config.rangingTest(&measure,false);
+  currentBtnState = digitalRead(BTN_PIN);
+
+  if(lastBtnState == HIGH && currentBtnState == LOW){
+    temp_state = !temp_state;
+    delay(50);
+  }
+  lastBtnState = currentBtnState;
+
+  // VL53L0X_RangingMeasurementData_t measure;
+
+  // sensor_config.rangingTest(&measure,false);
 
   display.clearDisplay();
   display.setTextSize(1);
   display.setCursor(0, 0);
+
+
+  if(temp_state == false){
+    float h = dht.readHumidity();
+    float t = dht.readTemperature();
+
+    display.println("DHT SENSOR\n");
+    
+    if(isnan(h) || isnan(t)){
+      display.println("Failed to read\n from DHT Sensor");
+    } else{
+      display.print("Temp: ");
+      display.print(t);
+      display.print(" °C");
+      
+      display.println("");
+
+      display.print("Hum: ");
+      display.print(h);
+      display.print(" %");
+    }
+
+  } else{
+    int irStatus = digitalRead(IR_PIN);
+
+    display = 
+  }
+
   display.print("Object Distance: ");
 
   display.setCursor(0, 24);
